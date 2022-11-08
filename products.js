b8eb43b6ff078519e30b69e7ff60edb8b0fb4d8d
Sami <sxmizaman@gmail.com>
2022-11-08 10:49:54 +0400

    updated as per week 7

---
+++ products.js
@@ -0,0 +1,56 @@
+let products = [{
+    id: 1001,
+    title: "PS2",
+    description:
+      "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+    price: 1000,
+    image:
+      "https://static.wikia.nocookie.net/seuss/images/9/9a/Img_drseuss_cat.gif",
+    availableInventory: 10,
+    rating: 1,
+  },
+  {
+    id: 1002,
+    title: "PS1",
+    description:
+      "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+    price: 20,
+    image:
+      "https://static.wikia.nocookie.net/seuss/images/9/9a/Img_drseuss_cat.gif",
+    availableInventory: 10,
+    rating: 2,
+  },
+  {
+    id: 1003,
+    title: "PS3",
+    description:
+      "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+    price: 200,
+    image:
+      "https://static.wikia.nocookie.net/seuss/images/9/9a/Img_drseuss_cat.gif",
+    availableInventory: 10,
+    rating: 3,
+  },
+  {
+    id: 1004,
+    title: "PS4",
+    description:
+      "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+    price: 100,
+    image:
+      "https://static.wikia.nocookie.net/seuss/images/9/9a/Img_drseuss_cat.gif",
+    availableInventory: 10,
+    rating: 4,
+  },
+  {
+    id: 1005,
+    title: "PS5",
+    description:
+      "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+    price: 10,
+    image:
+      "https://static.wikia.nocookie.net/seuss/images/9/9a/Img_drseuss_cat.gif",
+    availableInventory: 10,
+    rating: 5,
+  }
+]
